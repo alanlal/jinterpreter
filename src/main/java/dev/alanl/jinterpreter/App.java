fbcb11fb0d0cac9db94264372b0c82cfe7f60f30
alanlal <alanlal249@gmail.com>
2023-10-08 21:12:55 +0530

    add a read eval print loop

--- src/main/java/dev/alanl/jinterpreter/App.java
+++ src/main/java/dev/alanl/jinterpreter/App.java
@@ -1,9 +1,26 @@
 package dev.alanl.jinterpreter;
 
+import dev.alanl.jinterpreter.ast.AstPrinter;
+
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">>");
+        while (scanner.hasNextLine()) {
+            String expression = scanner.nextLine();
+            System.out.println(expression);
+            dev.alanl.jinterpreter.Scanner codeScanner = new dev.alanl.jinterpreter.Scanner(expression);
+            List<Token> tokens = codeScanner.scanTokens();
+            Parser parser = new Parser(tokens);
+            AstPrinter astPrinter = new AstPrinter();
+            System.out.println(astPrinter.print(parser.expression()));
+            System.out.print(">>");
+        }
     }
 }
